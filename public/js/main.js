1b5aeed5eed4cae3e0399dd56f49ba3a995b1870
jose bedon <jbedonsanchez92@gmail.com>
2014-05-08 09:31:37 -0500

    server socket io server html side added

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,44 @@
 var app = angular.module("chatApp",[]);
 
-app.factory("socket",function(){
+/*app.factory("socket",function(){
 	var socket=io.connect("http://localhost:3000");
 	socket.emit("connect");
 	return socket;
-});
+});*/
 
-app.controller("ChatCrtl",function($scope,socket){
+app.controller("ChatCrtl",function($scope){
 
 	$scope.msgs=[];
 	$scope.logmsgs=[];
+
+	var socket=null;
 
 	$scope.sendMsg = function(){
 		socket.emit("message","html client : "+$scope.msg.text);
 		$scope.msg.text="";
 		$scope.count++;
 	}
-	
-	socket.on("established",function(data){
+
+	$scope.connectSocketIo = function(){
+		socket=io.connect($scope.server);
+		socket.emit("connect");
+
+		socket.on("established",ESTABLISHED);
+		socket.on("message",MESSAGE);
+
+
+	}
+
+	function ESTABLISHED(data){
 		$scope.logmsgs.push(data);
 		$scope.$digest();
-	});
-	
-	
-	socket.on("message",function(data){
+	}
+
+	function MESSAGE(data){
 		$scope.msgs.push(data);
 		$scope.$digest();
-	});
+	}
+	
+
 	
 });
